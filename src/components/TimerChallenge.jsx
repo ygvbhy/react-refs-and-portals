c098362488cbcb9712962b102d99e7a7110f5694
ygvbhy <ygvbhy@naver.com>
2024-12-03 23:07:47 +0900

    타이머 설정 & State 관리

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TimerChallenge = ({ title, targetTime }) => {
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  const handleStart = () => {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  };
+
   return (
     <div className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} seconds{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? "active" : ""}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </div>
   );
 };
