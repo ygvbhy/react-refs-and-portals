2b93ebaaf01d0c73015b19357780286197dd727b
ygvbhy <ygvbhy@naver.com>
2024-12-03 22:50:09 +0900

    Refs 로 HTML 요소 접근

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  const handleClick = () => {
+    // 참조를 통해 플레이어 이름을 업데이트 함
+    // onChange 로 클릭 될때마다 하지 않아도 됨
+    setEnteredPlayerName(playerName.current.value);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
