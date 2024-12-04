a7a5416e10229b60d19fe2aefe6fdc1ce5bc739e
sypark <sangyune.park@k-futuretech.com>
2024-12-04 16:56:29 +0900

    Refs와 State 를 사용해야 하는 경우

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -6,38 +6,50 @@ const TimerChallenge = ({ title, targetTime }) => {
   // 그래서 참조 값으로 따로 선언 해서 진행 해야 함
   const timer = useRef();
   const dialog = useRef();
-  const [timerExpired, setTimerExpired] = useState(false);
-  const [timerStarted, setTimerStarted] = useState(false);
+  // const [timerExpired, setTimerExpired] = useState(false);
+  // const [timerStarted, setTimerStarted] = useState(false);
+
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    setInterval(timer.current);
+    setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
+  }
 
   const handleStart = () => {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
+    timer.current = setInterval(() => {
+      // setTimerExpired(true);
       // 자식 컴포넌트에서 받아온 ref 함수
-      dialog.current.open();
-    }, targetTime * 1000);
+      // dialog.current.open();
+      setTimeRemaining((prevTimeRemaing) => prevTimeRemaing - 10);
+    }, 10);
 
-    setTimerStarted(true);
+    // setTimerStarted(true);
   };
 
   const handleStop = () => {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   };
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
-      <div className="challenge">
+      <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
+      <div className='challenge'>
         <h2>{title}</h2>
-        <p className="challenge-time">
+        <p className='challenge-time'>
           {targetTime} seconds{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : ""}>
-          {timerStarted ? "Time is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : ""}>
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
         </p>
       </div>
     </>
